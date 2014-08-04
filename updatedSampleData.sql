4f8e13e6c8328318140cef5aea09a39dcfabe675
Jon Lee <jonlee@Jons-MacBook-Pro.local>
2014-08-03 23:53:43 -0400

    Data

---
+++ updatedSampleData.sql
@@ -0,0 +1,26 @@
+
+insert into usertypeid values(1, 'Student');
+insert into usertypeid values(2, 'Professor');
+insert into usertypeid values(3, 'Admin');
+
+insert into majors values('198', 'Computer Science');
+insert into majors values('640', 'Math');
+insert into majors values('730', 'Physics');
+
+insert into users values('123456789', 'Jon Lee', 'jl1424', 'root', 1);
+insert into users values('234567891', 'root', 'root', 'root', 1);
+insert into users values('345678912', 'groot', 'groot', 'groot', 2);
+insert into users values('456789123', 'Admin', 'admin', 'admin', 3);
+
+insert into students values('123456789', 'jl1424', 'Jon Lee', '198');
+insert into students values('234567891', 'root', 'root', '640');
+
+insert into classrooms values('SEC', 60, '208');
+insert into classrooms values('TIL', 60, '210');
+insert into classrooms values('ENG', 120, '111');
+
+insert into courses values('198', '111', '01', 'FA', '2014', '345678912','ENG', '111', 120, '2014-9-21', null);  
+insert into courses values('198', '112', '01', 'FA', '2014', '345678912', 'ENG', '111', 120, '2014-9-21', '198:111');  
+insert into courses values('198', '211', '01', 'FA', '2014', '345678912','ENG', '111', 120, '2014-9-21', '198:112');  
+
+
